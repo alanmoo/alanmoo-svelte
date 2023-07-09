18ca678e5c094486ceebddf81cc599a8176ee0a8
Alan Mooiman <amooiman@gmail.com>
2023-07-09 18:47:54 -0400

    Add Vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import Header from "$lib/components/header.svelte"
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+  
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 <Header></Header>
 <main>
