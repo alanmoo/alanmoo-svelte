9309d21407c21a517c502fbc85a9351ab36e34fe
Alan Mooiman <amooiman@gmail.com>
2023-09-05 22:24:20 -0400

    New blog post

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -10,4 +10,20 @@
   :global(p) {
     line-height: 1.5;
   }
+
+  :global(pre) {
+    background: var(--theme-dark);
+    color: var(--theme-light);
+    max-width: 100vw;
+    border: 1px solid var(--theme-brown);
+    padding: 1em;
+    text-wrap: wrap;
+  }
+
+  :global(code){
+    color: var(--theme-light);
+    background: var(--theme-dark);
+  }
+
+
 </style>
