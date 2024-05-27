cc4bdfb85e499f3e1652fb72631941ce4c3a1e0e
Alan Mooiman <amooiman@gmail.com>
2024-05-27 18:38:35 -0400

    Adjust title

--- src/routes/resume/+layout.svelte
+++ src/routes/resume/+layout.svelte
@@ -3,7 +3,7 @@
 		name="description"
 		content="Alan Mooiman is a software engineering leader and an advocate for an people first, accessible world, both online and offline"
 	/>
-	<title>Alan Mooiman | User-focused engineering manager</title>
+	<title>Alan Mooiman | User-focused web engineer</title>
 </svelte:head>
 
 <slot />
